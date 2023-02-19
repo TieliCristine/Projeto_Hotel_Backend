7d5e4919719e09eeb0fae62a95b40aebc7195d84
Tieli Cristine <tielicristinesilva@gmail.com>
2023-02-19 14:03:03 -0300

    refatoração CustomerController e CustomerService

--- src/main/java/com/projeto/hotel/controller/CustomerController.java
+++ src/main/java/com/projeto/hotel/controller/CustomerController.java
@@ -1,31 +1,25 @@
 package com.projeto.hotel.controller;
 
 import com.projeto.hotel.model.entity.Customer;
-import com.projeto.hotel.model.repository.CustomerRepository;
 import com.projeto.hotel.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.ErrorResponseException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("api/customer")
 public class CustomerController {
 
-    @Autowired
     private CustomerService customerService;
 
-    @Autowired
-    private CustomerRepository customerRepository;
-
-    @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public Customer save(@RequestBody Customer customer) {
-        return customerService.save(customer);
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
     }
 
     @GetMapping
@@ -33,48 +27,32 @@ public class CustomerController {
         return customerService.list();
     }
 
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Customer save(@RequestBody @Valid Customer customer) {
+        return customerService.save(customer);
+    }
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Customer> findById(@PathVariable Long id) {
-        return customerRepository.findById(id)
+    public ResponseEntity<Customer> findById(@PathVariable @NotNull @Positive Long id) {
+        return customerService.findById(id)
                 .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Customer> update(@PathVariable Long id, @RequestBody Customer customer) {
-        return customerRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setCpf(customer.getCpf());
-                    recordFound.setName(customer.getName());
-                    recordFound.setBirthdate(customer.getBirthdate());
-                    recordFound.setGender(customer.getGender());
-                    recordFound.setMobile(customer.getMobile());
-                    recordFound = customerService.update(recordFound);
-//                    customerService.update(recordFound);
-                    Customer updated = customerService.save(recordFound);
-                    return ResponseEntity.ok().body(updated);
-                })
+    public ResponseEntity<Customer> update(@PathVariable  @NotNull @Positive Long id, @RequestBody @Valid Customer customer) {
+        return customerService.update(id, customer)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        return customerRepository.findById(id)
-                .map(recordFound -> {
-           customerRepository.deleteById(id);
-            return ResponseEntity.noContent().<Void>build();
-        })
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Void> delete(@PathVariable  @NotNull @Positive Long id) {
+        if (customerService.delete(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
-
-//    @GetMapping(value = "/{cpf}")
-//    public Customer findByCpf(@PathVariable String cpf) {
-//        return customerService.findByCpf(cpf);
-//    }
-//
-//    @PutMapping(value = "/{cpf}")
-//    public Customer updateByCpf(@RequestBody Customer customer) {
-//        return customerService.save(customer);
-//    }
 }
--- src/main/java/com/projeto/hotel/model/entity/Customer.java
+++ src/main/java/com/projeto/hotel/model/entity/Customer.java
@@ -1,11 +1,19 @@
 package com.projeto.hotel.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
+@SQLDelete(sql = "UPDATE Customer SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 @Entity
 @Table(name = "Customer")
 public class Customer {
@@ -14,21 +22,38 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @NotNull
+    @Length(min = 11, max = 11)
     @Column(unique = true, length = 11, nullable = false)
     private String cpf;
 
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 225)
     @Column(length = 225, nullable = false)
     private String name;
 
+    @NotNull
     @Column(nullable = false)
     private Date birthdate;
 
+    @NotNull
+    @Pattern(regexp = "Feminino|Masculino|Não Informar")
     @Column(length = 12, nullable = false)
     private String gender;
 
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 225)
     @Column(length = 12, nullable = false)
     private String mobile;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 
 
 //    @NotNull
